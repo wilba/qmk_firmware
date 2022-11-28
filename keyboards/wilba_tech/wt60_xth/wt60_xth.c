3be54d6f57a4181231849b7e8bf46f726b6c55fd
wilba <wilba@wilba.tech>
2022-11-29 02:42:31 +1100

    Remove caps lock LED

--- keyboards/wilba_tech/wt60_xth/wt60_xth.c
+++ keyboards/wilba_tech/wt60_xth/wt60_xth.c
@@ -56,10 +56,6 @@ void keyboard_pre_init_kb(void) {
 }
 
 bool led_update_kb(led_t led_state) {
-    if (led_update_user(led_state)) {
-        writePin(F1, led_state.caps_lock);
-    }
-
 #ifdef AUDIO_ENABLE
     static led_t old_led_state = { .raw = 0 };
 

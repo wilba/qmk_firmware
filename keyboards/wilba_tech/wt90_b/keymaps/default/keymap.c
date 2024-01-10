c60c94dfd8009d9b966cd100ca631d6df62f4391
wilba <wilba@wilba.tech>
2024-01-10 20:08:39 +1100

    switch to RGB_MATRIX

--- keyboards/wilba_tech/wt90_b/keymaps/default/keymap.c
+++ keyboards/wilba_tech/wt90_b/keymaps/default/keymap.c
@@ -1,19 +1,7 @@
-/* Copyright 2020 Jason Williams (Wilba)
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-// Default layout for WT80-G
+// Copyright 2023 Jason Williams (@wilba)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+// Default layout for WT90-B
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
